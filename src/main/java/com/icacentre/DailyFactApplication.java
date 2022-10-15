f7b5221f7dee915c6a7030d7b3c57bab12130542
MohdFaijan <MohdFaijan@DESKTOP-BTUOERV.mshome.net>
2022-10-15 14:36:58 +0530

    Added src folder and pom file

---
+++ src/main/java/com/icacentre/DailyFactApplication.java
@@ -0,0 +1,13 @@
+package com.icacentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyFactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyFactApplication.class, args);
+	}
+
+}
