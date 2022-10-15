34d5289570bf0ca5c19a29a85d3728f3ebec56ec
MohdFaijan <MohdFaijan@DESKTOP-BTUOERV.mshome.net>
2022-10-15 15:15:47 +0530

    Added Home page msg

--- src/main/java/com/icacentre/controller/MyController.java
+++ src/main/java/com/icacentre/controller/MyController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+	@GetMapping("/")
+	public String getHome() {
+		return "Home Page";
+	}
 	@GetMapping("/getMsg")
 	public String getMsg() {
 		return "Hello! This is msg from my side";
